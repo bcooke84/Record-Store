56924af322f210a1f85d1822e92f9a5490c4e683
Brian Cooke <brian.cooke@live.co.uk>
2018-04-21 10:01:15 +0100

    Planning documents added, database table & sql_runnerl created.

---
+++ db/record_store.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE albums(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  stock_level INT,
+  artist_id INT REFERENCES artists(id) ON DELETE CASCADE
+);
