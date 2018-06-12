b5289cdcf1f19646758eeec1da64a30a01014497
Brian Cooke <brian.cooke@live.co.uk>
2018-06-12 23:19:20 +0100

    changed table headers to highlight entire container when selected

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -2,26 +2,9 @@ require( 'pg' )
 
 class SqlRunner
 
-  # def self.run(sql, values = [])
-  #   begin
-  #     db = PG.connect({ dbname: "record_store", host: "localhost" })
-  #     db.prepare("query", sql)
-  #     result = db.exec_prepared("query", values)
-  #   ensure
-  #     db.close()
-  #   end
-  #   return result
-  # end
-
   def self.run(sql, values = [])
     begin
-      db = PG.connect({
-        dbname: 'dbv52s116ktu48',
-        host: 'ec2-50-19-224-165.compute-1.amazonaws.com',
-        port: '5432',
-        user: 'qbxxhyyqzucndt',
-        password: '5842302e605ad132e921f5d7920b45273f19bd845027922d551c393075847c0d'
-        })
+      db = PG.connect({ dbname: "record_store", host: "localhost" })
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
@@ -29,5 +12,22 @@ class SqlRunner
     end
     return result
   end
+
+  # def self.run(sql, values = [])
+  #   begin
+  #     db = PG.connect({
+  #       dbname: 'dbv52s116ktu48',
+  #       host: 'ec2-50-19-224-165.compute-1.amazonaws.com',
+  #       port: '5432',
+  #       user: 'qbxxhyyqzucndt',
+  #       password: '5842302e605ad132e921f5d7920b45273f19bd845027922d551c393075847c0d'
+  #       })
+  #     db.prepare("query", sql)
+  #     result = db.exec_prepared("query", values)
+  #   ensure
+  #     db.close()
+  #   end
+  #   return result
+  # end
 
 end
